b5bd825141fb138c23a6f1a404dd6e3b6e0ff1db
Jon Deavers <jondeavers@gmail.com>
2020-09-22 21:19:46 -0400

    Update populateDOM.js

--- populateDOM.js
+++ populateDOM.js
@@ -1,20 +1,6 @@
-//variables needed
-var searchTerm
-var recordCount
-var startYear
-var endYear
+function PostArticles(){
+    for (var i =0; i < articles.length; i++) {
 
-//create the api call
-
-//for loop to pull multiple articles
-
-//ajax call to print to DOM
-.then(function(response){
-    var results =response.data;
-
-    for (var i = 0; i < recordCount.length; i++) {
-        var li = $("<li>");
-
-        var title = response[i].Title
+        $("#article-list").append(articles[i].Title+"  "+articles[i].Author+"  "+articles[i].articleUrl +"<br>");
     }
-});
+}
